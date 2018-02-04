500ca575e736a772f6457644721cba20b74eb39e
cerico <garethrobertlee@gmail.com>
2018-02-04 05:36:56 +0000

    backup zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ ln -s ~/dotfiles/zfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/
 mv ~/Library/Preferences/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist.bak
 ln -s ~/dotfiles/zfiles/Desktops/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mv  ~/.zshrc  ~/.zshrc.orig
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/zfiles ~/zfiles
