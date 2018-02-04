e922fe4b506fba2955839c016a8b9e2ebd50d112
cerico <garethrobertlee@gmail.com>
2018-02-04 11:14:28 +0000

    change bootstrap order

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,10 @@ ln -s ~/dotfiles/zfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/
 mv ~/Library/Preferences/com.apple.spaces.plist ~/orig
 ln -s ~/dotfiles/zfiles/Desktops/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
 mv  ~/.zshrc  ~/orig
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/zfiles ~/zfiles
-chsh -s /bin/zsh
+source ~/.zshrc
 
