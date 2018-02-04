494c8b947245e4f4e82dfa41ae688084ed98df05
cerico <garethrobertlee@gmail.com>
2018-02-04 14:48:40 +0000

    login-items

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,11 @@ mkdir ~/orig
 mv ~/Library/Preferences/com.googlecode.iterm2.plist ~/orig
 ln -s ~/dotfiles/zfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 
+# Login
+mv ~/Library/Preferences/com.apple.loginitems.plist ~/orig
+ln -s ~/dotfiles/zfiles/LoginItems/com.apple.loginitems.plist ~/Library/Preferences/com.apple.loginitems.plist
+
+
 # Spaces Preferences
 mv ~/Library/Preferences/com.apple.spaces.plist ~/orig
 ln -s ~/dotfiles/zfiles/Desktops/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist
