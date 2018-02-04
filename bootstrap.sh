caa923cf3dae75ce0dcba6bb9c5d69a7bd31bf11
cerico <garethrobertlee@gmail.com>
2018-02-04 05:23:10 +0000

    desktops

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,9 @@
 mv ~/Library/Preferences/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist.bak
 ln -s ~/dotfiles/zfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 mv ~/Library/Preferences/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist.bak
-ln -s ~/dotfiles/zfiles/iterm/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist
+ln -s ~/dotfiles/zfiles/Desktops/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist
 
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/zfiles ~/zfiles
-chsh -s /bin/zsh 
-source ~/.zshrc
+chsh -s /bin/zsh
