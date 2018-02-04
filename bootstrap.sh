267cc3f13a4f5b9ace6db2c341c4b1326faaf5ce
cerico <garethrobertlee@gmail.com>
2018-02-04 05:44:38 +0000

    orig folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
-mv ~/Library/Preferences/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist.bak
+mkdir ~/orig
+mv ~/Library/Preferences/com.googlecode.iterm2.plist ~/orig
 ln -s ~/dotfiles/zfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
-mv ~/Library/Preferences/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist.bak
+mv ~/Library/Preferences/com.apple.spaces.plist ~/orig
 ln -s ~/dotfiles/zfiles/Desktops/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mv  ~/.zshrc  ~/.zshrc.orig
+mv  ~/.zshrc  ~/orig
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/zfiles ~/zfiles
