c49b43a1635f154003d2dd8c571fec96c296c332
cerico <garethrobertlee@gmail.com>
2018-02-04 13:51:28 +0000

    use modified oh-my-zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@ mv ~/Library/Preferences/com.googlecode.iterm2.plist ~/orig
 ln -s ~/dotfiles/zfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 mv ~/Library/Preferences/com.apple.spaces.plist ~/orig
 ln -s ~/dotfiles/zfiles/Desktops/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist
-ln -s ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/zfiles ~/zfiles
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/cerico/dotfiles/master/zfiles/oh-my-zsh/scripted-install.sh)"
 source ~/.zshrc
