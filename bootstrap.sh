8b13db26c8c6e7e3f81f1fc2b99b4e1f223dd77c
cerico <garethrobertlee@gmail.com>
2018-02-04 16:44:05 +0000

    pics

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,9 @@
 mkdir ~/orig
 
-# Iterm Preferences
-mv ~/Library/Preferences/com.googlecode.iterm2.plist ~/orig
-ln -s ~/dotfiles/zfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
-
-# Login
-mv ~/Library/Preferences/com.apple.loginitems.plist ~/orig
-ln -s ~/dotfiles/zfiles/LoginItems/com.apple.loginitems.plist ~/Library/Preferences/com.apple.loginitems.plist
-
-
-# Spaces Preferences
-mv ~/Library/Preferences/com.apple.spaces.plist ~/orig
-ln -s ~/dotfiles/zfiles/Desktops/com.apple.spaces.plist ~/Library/Preferences/com.apple.spaces.plist
-
-# Alfred Preferences
-mv ~/Library/Preferences/com.apple.systempreferences.plist ~/orig
-ln -s ~/dotfiles/zfiles/Alfred/com.apple.systempreferences.plist ~/Library/Preferences/com.apple.systempreferences.plist
-
-mv ~/Library/Preferences ~/orig
-ln -s ~/dotfiles/zfiles/Preferences ~/Library/Preferences
+mv ~/Library/Preferences/* ~/orig
+mv ~/Library/Application\ Support/Dock/desktoppicture.db ~/orig
+cp -r ~/dotfiles/zfiles/Preferences/* ~/Library/Preferences/*
+cp -r ~/dotfiles/zfiles/desktoppicture.db ~/Library/Application\ Support/Dock/desktoppicture.db
 
 # Main zsh/oh-my-zsh configuration. It installs from a modifies oh-my-zsh script, the only difference being it doesn't wipe out the exiting .zshrc as we want to keep our dotfiles .zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
