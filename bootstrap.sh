fba496845feff682d3d14f4745b28277e0877d08
cerico <garethrobertlee@gmail.com>
2018-02-05 12:59:16 +0000

    installations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
+# Dotfiles
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/zfiles ~/zfiles
+
 # Preferences for iterm, spaces, login items and dock
 
 cp -a ~/dotfiles/zfiles/wallpaper/Dock ~/Library/Application\ Support/
@@ -8,11 +13,3 @@ defaults import com.apple.loginitems.plist ~/dotfiles/zfiles/macos/com.apple.log
 defaults import com.apple.spaces.plist ~/dotfiles/zfiles/macos/com.apple.spaces.plist
 defaults import com.apple.dock.plist ~/dotfiles/zfiles/macos/com.apple.dock.plist
 defaults import com.apple.systempreferences.plist ~/dotfiles/zfiles/macos/com.apple.systempreferences.plist
-
-# Main zsh/oh-my-zsh configuration. It installs from a modifies oh-my-zsh script, the only difference being it doesn't wipe out the exiting .zshrc as we want to keep our dotfiles .zshrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.zprofile ~/.zprofile
-ln -s ~/dotfiles/zfiles ~/zfiles
-
-# Non destructive oh-my-zsh installation - it leaves zshrc alone!
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/cerico/dotfiles/master/zfiles/oh-my-zsh/scripted-install.sh)"
--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew tap jotta/cli
 brew install caskroom/cask/brew-cask tree docker-compose jotta-cli
 brew cask install vagrant virtualbox docker alfred google-chrome visual-studio-code firefox chrome-devtools
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
