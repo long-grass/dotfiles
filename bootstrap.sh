7d39ec716f2326215e0e03dad0d4324548e273ed
cerico <garethrobertlee@gmail.com>
2018-02-05 16:45:54 +0000

    spaces

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,9 +16,11 @@ fi
 
 cp -a ~/dotfiles/zfiles/wallpaper/Dock ~/Library/Application\ Support/
 
+defaults delete com.apple.spaces
 defaults delete com.apple.spaces.plist
 defaults import com.googlecode.iterm2.plist ~/dotfiles/zfiles/macos/com.googlecode.iterm2.plist
 defaults import com.apple.loginitems.plist ~/dotfiles/zfiles/macos/com.apple.loginitems.plist
+defaults import com.apple.spaces ~/dotfiles/zfiles/macos/com.apple.spaces.plist
 defaults import com.apple.spaces.plist ~/dotfiles/zfiles/macos/com.apple.spaces.plist
 defaults import com.apple.dock.plist ~/dotfiles/zfiles/macos/com.apple.dock.plist
 defaults import com.apple.systempreferences.plist ~/dotfiles/zfiles/macos/com.apple.systempreferences.plist
