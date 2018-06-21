7659536d7b05e03669ad56b38d525e3768e761a3
garethrobertlee <garethrobertlee@gmail.com>
2018-06-21 06:48:07 +0100

    ableton

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap jotta/cli
-brew install caskroom/cask/brew-cask pow qt redis tree docker-compose postgresql jotta-cli docker-machine ableton-live-suite
-brew cask install iterm2 vagrant vagrant-manager virtualbox virtualbox-extension-pack tunnelblick postman docker alfred google-chrome slack visual-studio-code firefox chrome-devtools
+brew install caskroom/cask/brew-cask pow qt redis tree docker-compose postgresql jotta-cli docker-machine
+brew cask install iterm2 vagrant vagrant-manager virtualbox virtualbox-extension-pack tunnelblick postman ableton-live-suite docker alfred google-chrome slack visual-studio-code firefox chrome-devtools
 rm -rf $(brew --cache)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
