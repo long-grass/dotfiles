7fdd9dd4f8d95baf58aac334b64d39aec5bbae1d
garethrobertlee <garethrobertlee@gmail.com>
2018-06-21 06:44:24 +0100

    cleanup brew cache after install

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew tap jotta/cli
 brew install caskroom/cask/brew-cask pow qt redis tree docker-compose postgresql jotta-cli docker-machine ableton-live-suite
 brew cask install iterm2 vagrant vagrant-manager virtualbox virtualbox-extension-pack tunnelblick postman docker alfred google-chrome slack visual-studio-code firefox chrome-devtools
+rm -rf $(brew --cache)
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
