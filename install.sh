27b309e79d601381bfeaae25f1428089ddf32ff1
garethrobertlee <garethrobertlee@gmail.com>
2018-06-21 06:54:19 +0100

    zsh should be last

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ brew install caskroom/cask/brew-cask pow qt redis tree docker-compose postgresql
 brew cask install iterm2 vagrant vagrant-manager virtualbox virtualbox-extension-pack tunnelblick postman ableton-live-suite docker alfred google-chrome slack visual-studio-code firefox chrome-devtools
 rm -rf $(brew --cache)
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 export PATH="$HOME/.rbenv/bin:$PATH" && rbenv install 2.4.3
@@ -14,3 +12,5 @@ rbenv global 2.4.3
 echo "gem: --no-document" > ~/.gemrc
 ~/.rbenv/shims/gem install bundler
 ~/.rbenv/shims/gem install rails
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
