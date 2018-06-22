ea6e8da9b0bfe8deb947eb2bbdb2087f4b74c494
cerico <garethrobertlee@gmail.com>
2018-06-22 07:11:05 +0100

    typo

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,6 @@
-xecutable File  21 lines (15 sloc)  879 Bytes
 if [ -d /Volumes/home ]; then
   sudo dscl . -change Users/$(id -F)  NFSHomeDirectory /Users/$(id -F) /Volumes/home
-  sudo mv /Users/$(id -F)/dotfiles /Volume/home/dotfiles
+  sudo mv /Users/$(id -F)/dotfiles /Volumes/home/dotfiles
   sudo rm -rf /Users/$(id -F)
   sudo reboot
 fi
