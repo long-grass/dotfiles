9a9dd736d2b5ee8c646fd661aa7bf24adaa098d6
cerico <garethrobertlee@gmail.com>
2019-11-19 20:28:22 +0000

    update nameserver

--- zfiles/scripts/setup-sites.sh
+++ zfiles/scripts/setup-sites.sh
@@ -4,6 +4,7 @@ ip=$(ifconfig | grep en0 -A99 | grep -w inet -m 1| awk -F' ' '{print $2}')
 echo "ip is now $ip"
 echo "nameserver $ip" > /etc/resolver/test
 echo "address=/.test/$ip" > /usr/local/etc/dnsmasq.d/development.conf
+networksetup -setdnsservers Wi-Fi $ip 8.8.8.8
 sudo brew services restart dnsmasq
 curl http://hello.test
 
