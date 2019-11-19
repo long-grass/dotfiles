83552dd5581ef45899da943e61b878efff65ebe7
cerico <garethrobertlee@gmail.com>
2019-11-19 20:13:21 +0000

    get eth correctly

--- zfiles/scripts/setup-sites.sh
+++ zfiles/scripts/setup-sites.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-ip=$(ifconfig | grep en0 -A 2 | tail -1 | awk -F' ' '{print $2}')
-echo $ip
+#add this to your startup scripts via System Preferences / Users & Groups / Login items
+ip=$(ifconfig | grep en0 -A99 | grep -w inet -m 1| awk -F' ' '{print $2}')
+echo "ip is now $ip"
 echo "nameserver $ip" > /etc/resolver/test
 echo "address=/.test/$ip" > /usr/local/etc/dnsmasq.d/development.conf
 sudo brew services restart dnsmasq
+curl http://hello.test
+
