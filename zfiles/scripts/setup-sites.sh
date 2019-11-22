0a9bc611b129e8bed79e26636ac77106b98c903f
cerico <garethrobertlee@gmail.com>
2019-11-22 13:32:22 +0000

    nginx

--- zfiles/scripts/setup-sites.sh
+++ zfiles/scripts/setup-sites.sh
@@ -2,10 +2,11 @@
 #add this to your startup scripts via System Preferences / Users & Groups / Login items
 ip=$(ifconfig | grep en0 -A99 | grep -w inet -m 1| awk -F' ' '{print $2}')
 echo "ip is now $ip"
-echo "nameserver $ip" > /etc/resolver/test
 echo "address=/.test/$ip" > /usr/local/etc/dnsmasq.d/development.conf
 networksetup -setdnsservers Wi-Fi $ip 8.8.8.8
 sudo brew services restart dnsmasq
+sleep 3
 sudo killall -HUP mDNSResponder
 curl http://hello.test
+open -n -a "Firefox" --args "--new-window" "http://hello.test/web"
 
